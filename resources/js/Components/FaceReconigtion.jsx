c6fc8583b09885871c314e348a3ea50a252b17b4
CostaX549 <costasantos896@gmail.com>
2024-03-15 11:36:10 -0300

    Mudanças 15/05/2024

--- resources/js/Components/FaceReconigtion.jsx
+++ resources/js/Components/FaceReconigtion.jsx
@@ -1,8 +1,9 @@
 import React, { useEffect, useState, useRef } from 'react';
 import * as faceapi from 'face-api.js';
 
-export default function FaceRecognition({ width, height, onRecognitionSuccess, setImagem }) {
+export default function FaceRecognition({ onRecognitionSuccess }) {
     const [modelsLoaded, setModelsLoaded] = useState(false);
+    const [currentCamera, setCurrentCamera] = useState('user');
     const videoRef = useRef();
     const canvasRef = useRef();
 
@@ -23,7 +24,7 @@ export default function FaceRecognition({ width, height, onRecognitionSuccess, s
 
     const startVideo = () => {
         navigator.mediaDevices
-            .getUserMedia({ video: { width: 300 } })
+            .getUserMedia({ video: { facingMode: currentCamera } })
             .then(stream => {
                 let video = videoRef.current;
                 if (video) {
@@ -36,6 +37,11 @@ export default function FaceRecognition({ width, height, onRecognitionSuccess, s
             });
     };
 
+    const toggleCamera = () => {
+        setCurrentCamera(currentCamera === 'user' ? 'environment' : 'user');
+        startVideo();
+    };
+
     const handleVideoOnPlay = () => {
         setInterval(faceMyDetect, 2500);
     };
@@ -45,8 +51,8 @@ export default function FaceRecognition({ width, height, onRecognitionSuccess, s
             const video = videoRef.current;
             canvasRef.current.innerHTML = faceapi.createCanvasFromMedia(video);
             const displaySize = {
-                width: width,
-                height: height
+                width: video.clientWidth,
+                height: video.clientHeight
             };
     
             faceapi.matchDimensions(canvasRef.current, displaySize);
@@ -58,44 +64,32 @@ export default function FaceRecognition({ width, height, onRecognitionSuccess, s
             if (canvasRef.current) {
                 const context = canvasRef.current.getContext('2d');
                 if (context) {
-                    context.clearRect(0, 0, width, height);
+                    context.clearRect(0, 0, displaySize.width, displaySize.height);
                     faceapi.draw.drawDetections(canvasRef.current, resizedDetections);
                     faceapi.draw.drawFaceLandmarks(canvasRef.current, resizedDetections);
                     faceapi.draw.drawFaceExpressions(canvasRef.current, resizedDetections);
     
                     if (detections.length > 0) {
-                 
-
                         const detection = detections[0]; // Apenas a primeira detecção
                         const confidence = detection.detection.score;
                         const box = detection.detection.box;
-                    
     
-                      
                         if (confidence >= 0.7) {
-                        
-                        const faceCanvas = document.createElement('canvas');
-                        faceCanvas.width = box.width;
-                        faceCanvas.height = box.height;
-                        const faceContext = faceCanvas.getContext('2d');
+                            const faceCanvas = document.createElement('canvas');
+                            faceCanvas.width = box.width;
+                            faceCanvas.height = box.height;
+                            const faceContext = faceCanvas.getContext('2d');
     
-                        // Desenha apenas o rosto no novo canvas
-                        faceContext.drawImage(video, box.x, box.y, box.width, box.height, 0, 0, box.width, box.height);
+                            // Desenha apenas o rosto no novo canvas
+                            faceContext.drawImage(video, box.x, box.y, box.width, box.height, 0, 0, box.width, box.height);
     
-                        // Obtém a imagem do rosto detectado como uma URL de dados
-                        const faceImageDataURL = faceCanvas.toDataURL('image/jpeg');
+                            // Obtém a imagem do rosto detectado como uma URL de dados
+                            const faceImageDataURL = faceCanvas.toDataURL('image/jpeg');
     
-                         
-                       
-                        
-        
-                         onRecognitionSuccess(faceImageDataURL);
-     
-      
+                            onRecognitionSuccess(faceImageDataURL);
                         } else {
                             console.log('Arrume a postura');
                         }
-                      
                     }
                 }
             }
@@ -105,8 +99,9 @@ export default function FaceRecognition({ width, height, onRecognitionSuccess, s
     return (
         <div className="flex justify-center">
             <div className="relative">
-                <video ref={videoRef} height={height} width={width} onPlay={handleVideoOnPlay} className="rounded-lg" />
-                <canvas ref={canvasRef} className="absolute inset-0" />
+                <video ref={videoRef} onPlay={handleVideoOnPlay} className="rounded-lg w-full h-auto" />
+                <canvas ref={canvasRef} className="absolute inset-0 w-full h-full" />
+                <button onClick={toggleCamera} className="absolute bottom-4 left-1/2 transform -translate-x-1/2 bg-white p-2 rounded-lg shadow">Virar</button>
             </div>
         </div>
     );
--- vite.config.js
+++ vite.config.js
@@ -10,4 +10,15 @@ export default defineConfig({
         }),
         react(),
     ],
+
+    watch: {
+        usePolling: true,
+        origin: 'http://192.168.0.72'
+    },
+    server: {
+        hmr: {
+            host: '192.168.0.72'
+        }
+    }
+
 });
